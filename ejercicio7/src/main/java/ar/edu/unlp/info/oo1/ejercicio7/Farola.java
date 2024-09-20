5a36afd82e7c0fcf77ca237bc042e0b90c15bf22
camilo <7camiloherrera@gmail.com>
2024-09-20 13:20:43 -0300

    comienzo eje7

---
+++ ejercicio7/src/main/java/ar/edu/unlp/info/oo1/ejercicio7/Farola.java
@@ -0,0 +1,53 @@
+package ar.edu.unlp.info.oo1.ejercicio7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Farola {
+	private boolean farola;
+	private List<Farola> listaVecinos;
+	
+	public Farola() {
+		this.farola = false;
+		this.listaVecinos = new ArrayList<Farola>();
+	}
+	
+	/*
+	* Crea la relación de vecinos entre las farolas. La relación de vecinos entre las farolas es recíproca, es decir el receptor del mensaje será vecino de otraFarola, al igual que otraFarola también se convertirá en vecina del receptor del mensaje
+	*/
+	public void pairWithNeighbor( Farola otraFarola ) {
+		if(!this.listaVecinos.contains(otraFarola)) { // si mi lista de vecino no contiene a la otra farola
+			this.listaVecinos.add(otraFarola); // la agrego a mi lista de vecinos
+			otraFarola.pairWithNeighbor(this); // hago que la otra farola me agregue a mi (seria lo recíproco)
+		}
+	}
+	/*
+	* Retorna sus farolas vecinas
+	*/
+	public List<Farola> getNeighbors (){
+		return listaVecinos;
+	}
+
+
+	/*
+	* Si la farola no está encendida, la enciende y propaga la acción.
+	*/
+	public void turnOn() {
+		
+	}
+
+	/*
+	* Si la farola no está apagada, la apaga y propaga la acción.
+	*/
+	public void turnOff() {
+		
+	}
+	
+	/*
+	* Retorna true si la farola está encendida.
+	*/
+	public boolean isOn() {
+		
+	}
+
+}
