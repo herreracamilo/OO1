9187c59350ebb096e11c6ab3f8279b89898a77ea
camilo herrera <7camiloherrera@gmail.com>
2024-10-05 19:56:29 -0300

    eje14 con sus test correspondientes

---
+++ ejercicio14/src/main/java/ar/edu/unlp/info/oo1/ejercicio14/DateLapse.java
@@ -0,0 +1,42 @@
+package ar.edu.unlp.info.oo1.ejercicio14;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class DateLapse {
+	private LocalDate from;
+	private LocalDate to;
+	
+	
+	public DateLapse(LocalDate from, LocalDate to) {
+		this.from = from;
+		this.to = to;
+	}
+
+
+	public LocalDate getFrom() { // “Retorna la fecha de inicio del rango”
+		return this.from;
+	}
+	
+
+	public LocalDate getTo() { //“Retorna la fecha de fin del rango”
+		return this.to;
+	}
+	
+
+	public int sizeInDays() { // “Retorna la cantidad de días entre la fecha 'from' y la fecha 'to'”
+		long cantidad = this.from.until(to, ChronoUnit.DAYS);
+		return (int) cantidad; // tengo que castear
+	}
+	
+
+	public boolean includesDate(LocalDate other) { //“Recibe un objeto LocalDate y retorna true si la fecha está entre el from y el to del receptor y false en caso contrario”.
+		boolean esta=false;
+		if(other.isAfter(this.from) && other.isBefore(this.to)) {
+			esta=true;
+		}
+		return esta;
+	}
+	
+
+}
---
+++ ejercicio14/src/test/java/ar/edu/unlp/info/oo1/ejercicio14/DateLapseTest.java
@@ -0,0 +1,50 @@
+package ar.edu.unlp.info.oo1.ejercicio14;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Aca escribiremos los test de unidad para cada clase 
+ * 
+ */
+public class DateLapseTest {
+	
+	LocalDate inicio;
+	LocalDate fin;
+	DateLapse test;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		inicio = LocalDate.of(1972, 12, 15);
+		fin = LocalDate.of(2032, 12, 15);
+		
+		test = new DateLapse(inicio, fin);
+	}
+	
+    @Test
+    public void testGetFrom() {
+    	LocalDate resultado = LocalDate.of(1972, 12, 15);
+        assertEquals(resultado, test.getFrom());
+    }
+    
+    @Test
+    public void testGetTo() {
+    	LocalDate resultado = LocalDate.of(2032, 12, 15);
+    	assertEquals(resultado, test.getTo());
+    }
+    
+    @Test
+    public void testSizeInDays() {
+    	assertEquals(21915, test.sizeInDays());
+    }
+    
+    @Test
+    public void testIncludesDate() {
+    	LocalDate fecha = LocalDate.of(2001, 07, 15);
+    	assertEquals(true, test.includesDate(fecha));
+    }
+}
